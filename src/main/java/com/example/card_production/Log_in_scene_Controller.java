4a9df95257455a28e1823254dd333eca4296e717
akbor120806 <151809122+akbor120806@users.noreply.github.com>
2025-11-26 20:51:04 +0600

    comment

--- src/main/java/com/example/card_production/Log_in_scene_Controller.java
+++ src/main/java/com/example/card_production/Log_in_scene_Controller.java
@@ -1,14 +1,29 @@
 package com.example.card_production;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import jdk.internal.icu.text.NormalizerBase;
 
 public class Log_in_scene_Controller {
+
     @FXML
-    private Label welcomeText;
+    private TextField PasswordTestFild;
+    @FXML
+    private TextField userNameOrEmailTextFild;
 
     @FXML
     protected void onHelloButtonClick() {
+        NormalizerBase welcomeText = null;
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void signInOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void SignUpOnActionButton(ActionEvent actionEvent) {
     }
 }
--- src/main/java/com/example/card_production/Sign_Up_scene_Controller.java
+++ src/main/java/com/example/card_production/Sign_Up_scene_Controller.java
@@ -1,7 +1,28 @@
 package com.example.card_production;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class Sign_Up_scene_Controller
 {
     @javafx.fxml.FXML
+    private TextField MobileNumberTextFile;
+    @javafx.fxml.FXML
+    private TextField passwordTextFile;
+    @javafx.fxml.FXML
+    private TextField gmailTextFile;
+    @javafx.fxml.FXML
+    private TextField nameTextFile;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void logInOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void signUpOnActionButton(ActionEvent actionEvent) {
+    }
+}
