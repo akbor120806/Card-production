0385805f408edefcc7eb9aca65faf9132ea425d1
akbor120806 <151809122+akbor120806@users.noreply.github.com>
2025-11-28 01:45:46 +0600

    commit

--- src/main/java/com/example/card_production/Akbor/ManagingDirector_Dashboard.java
+++ src/main/java/com/example/card_production/Akbor/ManagingDirector_Dashboard.java
@@ -3,5 +3,45 @@ package com.example.card_production.Akbor;
 public class ManagingDirector_Dashboard
 {
     @javafx.fxml.FXML
+    private TextField tytelShowLogin;
+    @javafx.fxml.FXML
+    private TextArea notificationTextArea;
+    @javafx.fxml.FXML
+    private TextField nameShowLogin;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void staffPerformanceOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void marketingOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void production_PlanOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void budgetOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void homeOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void selesReportOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clientOrderOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void OnActionButton(ActionEvent actionEvent) {
+    }
+}
